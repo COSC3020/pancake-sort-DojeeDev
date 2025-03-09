744e40889dfeb563d95a82512fba30132077527e
Ivan Martin <69719490+DojeeDev@users.noreply.github.com>
2025-03-09 17:28:45 -0600

    Documented code

--- code.js
+++ code.js
@@ -13,7 +13,8 @@ function flip(array, n) {
 // Use only flip() here to manipulate the array
 function pancakeSort(array) {
   for (var i=0; i < array.length; i++) {
-    for (var j=i; j > 0 && array[j-1] > array[j]; j--) {
+    for (var j=i; j > 0 && array[j-1] > array[j]; j--) { // same as insertion sort, insert element j into the correct postion moving to the left
+      // These 3 flips are the same as swaping array[j] and array[j-1]
       array = flip(array, j);
       array = flip(array, 1);
       array = flip(array, j);
